c197615d9a4cfb9bc5a7c80aaf62e1eea78b06dc
edel <edel.lock@if-core.de>
2013-07-25 15:04:52 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+//alle Eventhandler initialisieren ("der Mannschaft ihre Aufgaben geben")
+//wird unten in $(document).ready() als Parameter übergeben
+
+var init = function() {
+	
+};
+
