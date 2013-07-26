d0bf23c7f8f9c8063a5c031451270b115ed8454e
edel <edel.lock@if-core.de>
2013-07-26 13:31:48 +0200

    carousel aus bootstrap eingefügt

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-
-//alle Eventhandler initialisieren ("der Mannschaft ihre Aufgaben geben")
-//wird unten in $(document).ready() === die init-function als Parameter übergeben
+// alle Eventhandler initialisieren ("der Mannschaft ihre Aufgaben geben")
+// wird unten in $(document).ready() === die init-function als Parameter
+// übergeben
 
 var init = function() {
-	
+	$('.carousel').carousel();
 };
-
